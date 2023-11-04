1faac6a8b75b63d88339f0a139f0166f5aab6764
Koushik Annareddy Sreenath <askoushik4@gmail.com>
2023-11-04 12:38:23 -0400

    Summarising code

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 import openai
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import LanguageParser
+from langchain.text_splitter import CharacterTextSplitter, Language, RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain, LLMChain
 from langchain.chat_models import ChatOpenAI
-from htmlTemplates import bot_template,user_template,css
+from langchain.chains.summarize import load_summarize_chain
+from htmlTemplates import bot_template, user_template, css
+from langchain.prompts import PromptTemplate
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+
 
 def get_pdf_text(pdf_docs):
     ### returns a single string with all the raw text from pdfs
@@ -21,13 +28,46 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
+
+def get_python_loader():
+    loader = GenericLoader.from_filesystem("codes",
+                                           glob="*/",
+                                           suffixes=[".py"],
+                                           parser=LanguageParser(language=Language.PYTHON, parser_threshold=500), )
+    return loader.load()
+
+
+def summarise_file():
+    prompt_template = """Write a concise summary of the following:
+        "{text}"
+        CONCISE SUMMARY:"""
+    prompt = PromptTemplate.from_template(prompt_template)
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    stuff_chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
+
+    docs = get_python_loader()
+
+    print(docs)
+    print("waiting")
+    summary_text = stuff_chain.run(docs)
+    print("done")
+    f = open("summary/summary.txt", "a")
+    f.write(summary_text)
+    f.close()
+
+    return
+
+
 def get_text_chunks(raw_text):
     ## create a new instance 
-    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000,chunk_overlap=200, length_function=len)
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len)
 
-    chunks = text_splitter.split_text(raw_text) ## returns a list of chunks with each chunk size 100
+    chunks = text_splitter.split_text(raw_text)  ## returns a list of chunks with each chunk size 100
 
-    return chunks  
+    return chunks
+
 
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
@@ -35,9 +75,10 @@ def get_vectorstore(text_chunks):
 
     return vectorstore
 
+
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.1)
-    memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True) 
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     ## initialize the conversation chain
     converstation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
@@ -47,9 +88,10 @@ def get_conversation_chain(vectorstore):
 
     return converstation_chain
 
+
 def handle_user_input(user_question):
     print(st)
-    response = st.session_state.conversation({'question':user_question})
+    response = st.session_state.conversation({'question': user_question})
 
     # Update the chat history
     st.session_state.chat_history = response['chat_history']
@@ -69,7 +111,8 @@ def handle_user_input(user_question):
 
 def main():
     ## this allows langchain access to the access tokens.Since we are using langchain , follow the same variable format
-    load_dotenv()  
+    load_dotenv()
+
     st.set_page_config(page_title="ProductDoc",page_icon=":books:")
 
     st.write(css, unsafe_allow_html=True)
@@ -84,9 +127,13 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        pdf_docs = st.file_uploader("Upload your pdfs here and click on 'Process'", accept_multiple_files=True)   ## allows you to upload files 
+        pdf_docs = st.file_uploader("Upload your pdfs here and click on 'Process'", accept_multiple_files=True)   ## allows you to upload files
+        if st.button("Summarize"):
+            with st.spinner("Processing"):
+                summarise_file()
+
         if st.button("Process"):
-            with st.spinner("Processing"):    ## all the processes under this will be happening while a spinner spins - benefit of the user  
+            with st.spinner("Processing"):    ## all the processes under this will be happening while a spinner spins - benefit of the user
                 ## get pdf text
 
                 raw_text = get_pdf_text(pdf_docs)
@@ -97,13 +144,13 @@ def main():
                 ## create vector store
                 vectorstore = get_vectorstore(text_chunks)
 
-                ## semantic search 
+                ## semantic search
 
                 ## create conversation chain
                 #session state lets streamlit know that this variable should not be reinitialized in this session
                 # the reinitialization could be triggered each time we click on 'Process'
                 # if we set the session state we can also use it outside of scope (outside of 'Process')
-                # if we are using sessiom state initialize it before 
+                # if we are using sessiom state initialize it before
 
                 st.session_state.conversation = get_conversation_chain(vectorstore)
 
